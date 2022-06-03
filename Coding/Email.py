86a1822b95e5ceaef89a2941d6802d1ab6b41fa5
Ananya0812 <106728120+Ananya0812@users.noreply.github.com>
2022-06-03 12:03:29 +1000

    Create Email.py

---
+++ Coding/Email.py
@@ -0,0 +1,13 @@
+import smtplib
+​
+sender_email = "ananyashah0812.as@gmail.com"
+rec_email = "ananya.shah@myprosperity.com.au"
+password = input(str("Please enter your password: "))
+message = "Hey sent this using python!"
+​
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, password)
+print("Login success")
+server.sendmail(sender_email, rec_email, message)
+print("Email sent to", rec_email)
