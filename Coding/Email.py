316223e78c5e89d6d0cb14f91f61eddfd574a79c
Ananya0812 <106728120+Ananya0812@users.noreply.github.com>
2022-06-05 18:55:19 +1000

    Update Email.py

--- Coding/Email.py
+++ Coding/Email.py
@@ -1,10 +1,8 @@
 import smtplib
-​
 sender_email = "ananyashah0812.as@gmail.com"
 rec_email = "ananya.shah@myprosperity.com.au"
 password = input(str("Please enter your password: "))
 message = "Hey sent this using python!"
-​
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(sender_email, password)
